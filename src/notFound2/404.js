3e595540c4da1e6909f4036c827b20d1fce8a320
wangqingzhou <wangqingzhou311@live.com>
2022-09-19 19:03:28 +0800

    Default Changelist

---
+++ src/notFound2/404.js
@@ -0,0 +1,14 @@
+
+function App() {
+    return (
+        <div className="App">
+            <header>
+                <div>
+                    404
+                </div>
+            </header>
+        </div>
+    );
+}
+
+export default App;
